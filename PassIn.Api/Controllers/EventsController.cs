18c79857ad0e4812236a8efb28f7cd5134ccfe92
Augusto Lima Jardim <augustolimajardim@gmail.com>
2024-04-02 15:19:02 -0300

    Refatora o processo de lançar exceções

--- PassIn.Api/Controllers/EventsController.cs
+++ PassIn.Api/Controllers/EventsController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using PassIn.Application.UseCases.Events.Register;
 using PassIn.Communication.Requests;
 using PassIn.Communication.Responses;
-using PassIn.Exceptions;
 
 namespace PassIn.Api.Controllers;
 
@@ -14,45 +13,23 @@ public class EventsController : ControllerBase
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestEventJson request)
     {
-        try
-        {
-            var useCase = new RegisterEventUseCase();
+        var useCase = new RegisterEventUseCase();
 
-            var response = useCase.Execute(request);
+        var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
-        }
-        catch (PassInException ex)
-        {
-            return BadRequest(new ResponseErrorJson(ex.Message));
-        }
-        catch
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
-        }
+        return Created(string.Empty, response);
     }
 
-    [HttpPost]
+    [HttpGet]
     [Route("{id}")]
-    [ProducesResponseType(typeof(ResponseRegisterJsonEventJson), StatusCodes.Status201Created)]
-    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
-    public IActionResult GetById([FromBody] Guid id)
+    [ProducesResponseType(typeof(ResponseEventJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    public IActionResult GetById([FromRoute] Guid id)
     {
-        try
-        {
-            var useCase = new GetEventByIdUseCase();
+        var useCase = new GetEventByIdUseCase();
 
-            var response = useCase.Execute(id);
+        var response = useCase.Execute(id);
 
-            return Ok(response);    
-        }
-        catch (PassInException ex)
-        {
-            return NotFound(new ResponseErrorJson(ex.Message));
-        }
-        catch
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
-        }
+        return Ok(response);
     }
 }
